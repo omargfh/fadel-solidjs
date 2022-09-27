e12e0572c5a20419c0378ec1067c61b27157ba17
smz <samyz.k71@gmail.com>
2022-09-27 23:35:43 +0200

    make use of PWA for offline

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import solidLabels from 'babel-plugin-solid-labels'
 import unoCss from 'unocss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 //@ts-ignore
 const isDev = process.env.NODE_ENV !== 'production'
@@ -14,6 +15,32 @@ export default defineConfig({
       },
     }),
     unoCss(),
+    VitePWA({
+      injectRegister: 'inline',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: isDev,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Fadel',
+        short_name: 'Fadel',
+        description: 'Compare Multiple Images.',
+        theme_color: '#151518',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   server: {
     port: 3000,
